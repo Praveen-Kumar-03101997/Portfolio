8c58b2646f5e59bb41847aff8f05ad49bd003188
unknown <you@example.com>
2020-09-23 19:33:24 +0530

    my portfolio website

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template, url_for, request, redirect
+import requests
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+def write_to_file(data):
+    with open('Database.txt', mode='a') as database:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        file=database.write(f'\n{email},{subject},{message}')
+def write_to_csv(data):
+    with open('Database2.csv',newline='', mode='a') as database2:
+        email=data["email"]
+        subject=data["subject"]
+        message=data["message"]
+        csv_writer=csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+@app.route('/submit_form' , methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/Thank.html')
+    else:
+        return "something went wrong"
+
